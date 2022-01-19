46aeb4828940d3a51e866ed797b3aa281b3fe52f
Tony Morris <afmorris@users.noreply.github.com>
2022-01-19 18:41:15 -0500

    Update txt.tf

--- morriscloud-com/txt.tf
+++ morriscloud-com/txt.tf
@@ -14,14 +14,6 @@ locals {
     }, {
     id    = "spf",
     name  = "morriscloud.com",
-    value = "v=spf1 include:_spf.google.com ~all"
-    }, {
-    id    = "dmarc",
-    name  = "_dmarc",
-    value = "v=DMARC1; p=none; rua=mailto:tony@morriscloud.com"
-    }, {
-    id    = "dkim",
-    name  = "google._domainkey",
-    value = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlfaUPCv+GHceNOqFUntQK1ZjWQqpxDJ+YZ0HOrbUf2AOuyLzxpwzYAFQpGrrA/osGxhOU6r0C/h01h9KUhOHqiI2BxPU5n/81EF5sXecAupY5PfyWeY9WCo7YU5yhi2JtBp2DHorQuxT3YHw8mEjAJRc+IQZrYOFGTMEKaNcqKi/JHhfFgHL98ePeLmAgjVY/X0ajVE0IwrRGmF5zdf+0XEtBf99PvyujgAKurWeMJvFvx52S6Pd+HbKeKSRME590pKfRGPxvhoKJBSiaDoZRL068ZDQnxz9DMNN8MvCp5dGAXb9v1xYq6EYJrXs5y/WSisP1p2VbsOmPl/O9mQWZQIDAQAB"
-  }]
+    value = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+    }]
 }
