b623850f9920ac50a33d9f9c9967441030c1bc68
Tony Morris <afmorris@users.noreply.github.com>
2022-01-19 18:40:22 -0500

    Update mx.tf

--- morriscloud-com/mx.tf
+++ morriscloud-com/mx.tf
@@ -2,32 +2,20 @@ locals {
   mx = [{
     id              = "mx1",
     name            = "morriscloud.com",
-    value           = "alt1.aspmx.l.google.com",
+    value           = "isaac.mx.cloudflare.net",
     allow_overwrite = false,
-    priority        = 5
+    priority        = 41
     }, {
     id              = "mx2",
     name            = "morriscloud.com",
-    value           = "alt2.aspmx.l.google.com",
+    value           = "linda.mx.cloudflare.net",
     allow_overwrite = false,
-    priority        = 5
+    priority        = 94
     }, {
     id              = "mx3",
     name            = "morriscloud.com",
-    value           = "alt3.aspmx.l.google.com",
+    value           = "amir.mx.cloudflare.net",
     allow_overwrite = false,
-    priority        = 10
-    }, {
-    id              = "mx4",
-    name            = "morriscloud.com",
-    value           = "alt4.aspmx.l.google.com",
-    allow_overwrite = false,
-    priority        = 10
-    }, {
-    id              = "mx",
-    name            = "morriscloud.com",
-    value           = "aspmx.l.google.com",
-    allow_overwrite = false,
-    priority        = 1
-  }]
+    priority        = 68
+    }]
 }
